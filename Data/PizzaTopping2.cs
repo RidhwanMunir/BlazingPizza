3effede0b7d5a442cadf1f6f4e5181e70e491088
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-03-06 16:15:37 +0800

    Create PizzaTopping2.cs
    old pizzatopping class

---
+++ Data/PizzaTopping2.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazingPizza.Data;
+
+public class PizzaTopping2
+{
+    public int ToppingId { get; set; }
+    public int PizzaId { get; set; }
+    public string Name { get; set; }
+    public string Ingredients { get; set; }
+}
