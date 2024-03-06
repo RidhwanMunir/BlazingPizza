4d21c660312281c6fdbf1e78ec4f55b8069a4547
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-03-06 16:32:35 +0800

    Update Program.cs
    add directive
    Register the orderstate service
    Registers the new PizzaStoreContext2 and provides the filename for the SQLite database
    Get the PizzaStoreContext2 from the service provider
    Seed the PizzaStoreContext2 database

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 // allows the app to use the BlazingPizza service
 using BlazingPizza.Data;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting; 
-using System.Threading.Tasks;
-using System.Threading.Tasks.Dataflow;
+using BlazingPizza.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +14,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 // Register the pizzas service
 builder.Services.AddSingleton<PizzaService>();
+// Register the orderstate service
+builder.Services.AddScoped<OrderState>();
 
 // Add the AppState class
 builder.Services.AddScoped<PizzaSalesState>();
@@ -24,6 +25,7 @@ builder.Services.AddScoped<PizzaSalesState>();
 builder.Services.AddHttpClient();
 // registers the new PizzaStoreContext and provides the filename for the SQLite database
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddSqlite<PizzaStoreContext2>("Data Source=pizza2.db");
 
 var app = builder.Build();
 
@@ -41,16 +43,26 @@ app.MapFallbackToPage("/_Host");
 // Defines the default route, mapping URLs to {controller}/{action}/{id?}
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
-
-// Initialize the database
+// Initialize the databases
 var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 using (var scope = scopeFactory.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
-    if (db.Database.EnsureCreated())
+    // Get the PizzaStoreContext from the service provider
+    var db1 = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db1.Database.EnsureCreated())
     {
-        SeedData.Initialize(db);
+        // Seed the PizzaStoreContext database
+        SeedData.Initialize(db1);
+    }
+
+    // Get the PizzaStoreContext2 from the service provider
+    var db2 = scope.ServiceProvider.GetRequiredService<PizzaStoreContext2>();
+    if (db2.Database.EnsureCreated())
+    {
+        // Seed the PizzaStoreContext2 database
+        SeedData2.Initialize(db2); // Assuming you have a separate SeedData2 class for PizzaStoreContext2
     }
 }
+
 
 app.Run();
